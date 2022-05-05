db164be76d9af322e7b668324c5c42e9b7ebafff
Daniel Currey <spiceycurrey@gmail.com>
2022-05-04 19:51:47 -0700

    first commit!

---
+++ C#/(5kyu)Anagrams.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+//https://www.codewars.com/kata/523a86aa4230ebb5420001e1
+public static class Kata
+{
+    static string SortString(string input)
+    {
+        char[] characters = input.ToCharArray(0, input.Length);
+        Array.Sort(characters);
+        return new string(characters);
+    }
+
+    public static List<string> Anagrams(string word, List<string> words)
+    {
+        string sortedWord = SortString(word);
+        List<string> anagrams = new List<string>();
+
+        foreach (string listWord in words)
+        {
+            string sortedListWord = SortString(listWord);
+
+            if (String.Equals(sortedWord, sortedListWord))
+                anagrams.Add(listWord);
+        }
+        return anagrams;
+    }
+}
---
+++ C#/(6kyu)Playing-with-passphrases.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+// https://www.codewars.com/kata/559536379512a64472000053
+public class PlayPass
+{
+    // needs to be lowercase input
+    private static char CircularShift(char c, int n)
+    {
+        int offset = 26;
+        if (n > 0) offset *= -1;
+
+        if ((int)c + n > 122 || (int)c + n < 97)
+            return (char)((int)c + n + offset);
+
+        return (char)((int)c + n);
+    }
+
+    private static bool IsDigit(char c)
+    {
+        return ((int)c >= 48) && ((int)c <= 57);
+    }
+
+    private static bool IsLetter(char c)
+    {
+        char temp = Char.ToLower(c);
+        return ((int)temp >= 97) && ((int)temp <= 122);
+    }
+
+    public static string playPass(string s, int n)
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (IsDigit(s[i]))
+            {
+                result.Append(9 - (int)Char.GetNumericValue(s[i]));
+            }
+            else if (IsLetter(s[i]))
+            {
+                char temp = Char.ToLower(s[i]);
+                temp = CircularShift(temp, n);
+
+                if (i % 2 == 0) temp = Char.ToUpper(temp);
+                else temp = Char.ToLower(temp);
+
+                result.Append(temp);
+            }
+            else result.Append(s[i]);
+        }
+        return result.ToString();
+    }
+}
