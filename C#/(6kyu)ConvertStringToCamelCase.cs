ea1bf56e7e27fe61929567b87fa926c91023ed68
Daniel Currey <spiceycurrey@gmail.com>
2022-05-04 20:48:57 -0700

    first commit!

---
+++ C#/(6kyu)ConvertStringToCamelCase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+//https://www.codewars.com/kata/517abf86da9663f1d2000003
+public class Kata
+{
+  public static string ToCamelCase(string str)
+  {
+    StringBuilder sb = new StringBuilder();
+    
+    bool flag = false;
+    foreach (char c in str)
+    {
+      if (c == '-' || c == '_')
+      {
+        flag = true;
+        continue;
+      }
+      else if (flag)
+      {
+        sb.Append(Char.ToUpper(c));
+        flag = false;
+      }
+      else sb.Append(c);
+    }
+    
+    return sb.ToString();
+  }
+}
---
+++ C#/(7kyu)NiceArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+//https://www.codewars.com/kata/59b844528bcb7735560000a0
+public class Kata
+{
+  public static bool IsNice(int[] arr)
+  {
+    if (arr.Length == 0 || arr.Length == 1) return false;
+
+    Dictionary<int, bool> dict = new Dictionary<int, bool>();
+    foreach (int value in arr)
+    {
+        if (!dict.ContainsKey(value))
+            dict.Add(value, true);
+    }
+
+    // check if there exists a value in the array that doesnt have a +1 or -1 also in the dict
+    foreach (int arrayValue in arr)
+    {
+        if (!dict.ContainsKey(arrayValue - 1) && !dict.ContainsKey(arrayValue + 1))
+            return false;
+    }
+    return true;
+  }
+}
